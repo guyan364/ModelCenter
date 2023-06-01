5259ecc548f07667543edbb548da4ccf58a0f7cf
zpl <zhangpl21@mails.tsinghua.edu.cn>
2023-06-01 08:13:04 +0800

    feat(dataset): llama super glue dataset

---
+++ model_center/dataset/llamadataset/__init__.py
@@ -0,0 +1,13 @@
+from .superglue import *
+
+DATASET = {
+    "BoolQ": BoolQ_Dataset,
+    "CB": CB_Dataset,
+    "COPA": COPA_Dataset,
+    "MultiRC": MultiRC_Dataset,
+    "ReCoRD": ReCoRD_Dataset,
+    "RTE": RTE_Dataset,
+    "WiC": WiC_Dataset,
+    "WSC": WSC_Dataset,
+    "MNLI": MNLI_Dataset,
+}
